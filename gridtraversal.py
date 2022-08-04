2e760ee3b4e793a8d56ec8468fba12b80b04779a
amber1o <100294813+amber1o@users.noreply.github.com>
2022-08-04 10:29:43 -0400

    Update gridtraversal.py

--- gridtraversal.py
+++ gridtraversal.py
@@ -30,6 +30,7 @@ class Solution:
         for i in range(1, m):
             for j in range(1, n):
                 grid[i][j] = grid[i-1][j] + grid[i][j-1]
+        return grid[-1][-1]
 
 def main():
     num1 = int(input())
